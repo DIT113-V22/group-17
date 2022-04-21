d794a0e60c34afc06f48d9011695144a5284eee1
ErgimanGU <ergisenja@hotmail.com>
2022-04-21 18:02:34 +0200

    MQTT implementation
    Implemented and tested MQTT in the .ino file and tested it with the terminal
    
    Co-Authored-By: Aditya-Nair-gu <81224841+Aditya-Nair-gu@users.noreply.github.com>
    Co-Authored-By: Yajiri <90855500+Yajiri@users.noreply.github.com>
    Co-Authored-By: EliasLindL <102305456+EliasLindL@users.noreply.github.com>

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -1,5 +1,14 @@
 #include <Smartcar.h>
+#include <MQTT.h>
+#include <WiFi.h>
 
+
+WiFiClient net;
+
+MQTTClient mqtt;
+
+char ssid[] = " ";
+char pass[] = " ";
 const int TRIGGER_PIN           = 6; // D6
 const int ECHO_PIN              = 7; // D7
 const unsigned int MAX_DISTANCE = 300; //set the distance to 300
@@ -9,6 +18,9 @@ const int lDegrees = -75; // degrees to turn left
 const int rDegrees = 75;  // degrees to turn right
 bool flag = false;
 
+
+
+
 ArduinoRuntime arduinoRuntime;
 BrushedMotor leftMotor(arduinoRuntime, smartcarlib::pins::v2::leftMotorPins);
 BrushedMotor rightMotor(arduinoRuntime, smartcarlib::pins::v2::rightMotorPins);
@@ -16,30 +28,28 @@ DifferentialControl control(leftMotor, rightMotor);
 SimpleCar car(control);
 SR04 front(arduinoRuntime, TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
-void autoStop(){
-  char input = Serial.read();
+void autoStop(String message){
   const auto distance = front.getDistance();
   if(distance>0 and distance<150 and !flag){
     car.setSpeed(0);
    } 
-  
-  if(input == 'l'){
+  if(message == "l"){
     flag == false;
     car.setSpeed(fSpeed);
     car.setAngle(lDegrees);  
-  }else if(input == 'r'){
+  }else if(message == "r"){
     flag = false;
     car.setSpeed(fSpeed);
     car.setAngle(rDegrees);
-  }else if(input == 'f'){
+  }else if(message == "f"){
     flag = false;
     car.setSpeed(fSpeed);
     car.setAngle(0);
-  } else if(input == 'b'){
+  } else if(message == "b"){
     flag = true;
     car.setSpeed(bSpeed);
     car.setAngle(0);
-  }else if(input == 's'){
+  }else if(message == "s"){
     flag = false;
     car.setSpeed(0);
     car.setAngle(0);
@@ -47,14 +57,47 @@ void autoStop(){
  
 }
 
+const auto mqttBrokerUrl = "127.0.0.1";
+
 void setup()
 {
     Serial.begin(9600);
+
+  WiFi.begin(ssid, pass); 
+  mqtt.begin(mqttBrokerUrl, 1883, net);
+   
+  Serial.println("Connecting to WiFi...");
+  auto wifiStatus = WiFi.status();
+  while (wifiStatus != WL_CONNECTED && wifiStatus != WL_NO_SHIELD) {
+    Serial.println(wifiStatus);
+    Serial.print(".");
+    delay(1000);
+    wifiStatus = WiFi.status();
+  }
+
+
+Serial.println("Connecting to MQTT broker");
+  while (!mqtt.connect("arduino", "public", "public")) {
+    Serial.print(".");
+    delay(1000);
+  }
+  mqtt.subscribe("myfirst/test", 1);
+  mqtt.onMessage([](String topic, String message){
+    if(topic=="myfirst/test"){
+      autoStop(message);
+    }
+  });
 }
+  
 
 void loop()
 {
-  autoStop();
+    if (mqtt.connected()) {
+    mqtt.loop();
+    autoStop("");
+    }
+    
+    
     Serial.println(front.getDistance());
     delay(100);
 }
