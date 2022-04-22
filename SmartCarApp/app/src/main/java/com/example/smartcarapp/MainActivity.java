41c6c31377734c1845c422dcf2edf5cd54a12d6a
ErgimanGU <ergisenja@hotmail.com>
2022-04-22 11:44:33 +0200

    Implemented mqtt client libraries
    Added mqtt libraries for android studio

--- SmartCarApp/app/src/main/java/com/example/smartcarapp/MainActivity.java
+++ SmartCarApp/app/src/main/java/com/example/smartcarapp/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.smartcarapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void forward(View view) {
+
     }
 }
