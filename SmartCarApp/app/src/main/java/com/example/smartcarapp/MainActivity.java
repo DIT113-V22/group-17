a82932396d05db6624cd6f17b13fd5fc1d0b7c43
ErgimanGU <ergisenja@hotmail.com>
2022-04-23 13:11:32 +0200

    Mqtt and functioning arrows
    Implemented mqtt and the necessary functions for it to publish, subscribe and connect. Now the arrows are functional and the car moves to the direction specified.

--- SmartCarApp/app/src/main/java/com/example/smartcarapp/MainActivity.java
+++ SmartCarApp/app/src/main/java/com/example/smartcarapp/MainActivity.java
@@ -1,87 +1,69 @@
 package com.example.smartcarapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
-import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
-import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
-import org.eclipse.paho.client.mqttv3.MqttTopic;
-import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
-
-
-
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "SmartCarApp";
-    private static final String EXTERNAL_MQTT_BROKER = "aerostun.dev";
     private static final String LOCALHOST = "10.0.2.2";
     private static final String MQTT_SERVER = "tcp://" + LOCALHOST + ":1883";
+    private static final int QOS = 1;
+    private static final int IMAGE_WIDTH = 320;
+    private static final int IMAGE_HEIGHT = 240;
+
     private MqttClient mMqttClient;
     private boolean isConnected = false;
-    private static final int QOS = 1;
+    private ImageView mCameraView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mMqttClient = new MqttClient(getApplicationContext(), MQTT_SERVER, TAG);
+        mCameraView = findViewById(R.id.imageView);
+
         connectToMqttBroker();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
 
-    public void forward(View view) throws MqttException {
-        String broker = "tcp://localhost:1883";
-        String topicName = "test/topic";
-        int qos = 1;
-
-        MqttClient mqttClient = new MqttClient(broker, String.valueOf(System.nanoTime()));
-//Mqtt ConnectOptions is used to set the additional features to mqtt message
-
-        MqttConnectOptions connOpts = new MqttConnectOptions();
-
-        connOpts.setCleanSession(true); //no persistent session
-        connOpts.setKeepAliveInterval(1000);
-
-
-        MqttMessage message = new MqttMessage("Ed Sheeran".getBytes());
-        message.setQos(qos);     //sets qos level 1
-        message.setRetained(true); //sets retained message
-
-        MqttTopic topic2 = mqttClient.getTopic(topicName);
-
-        mqttClient.connect(connOpts); //connects the broker with connect options
-        topic2.publish(message);    // publishes the message to the topic(test/topic)
-        System.out.println("f");
+        connectToMqttBroker();
     }
 
-    public void reverse(View view) {
-        System.out.println("b");
-    }
+    @Override
+    protected void onPause() {
+        super.onPause();
 
-    public void turnRight(View view) {
-        System.out.println("r");
-    }
+        mMqttClient.disconnect(new IMqttActionListener() {
+            @Override
+            public void onSuccess(IMqttToken asyncActionToken) {
+                Log.i(TAG, "Disconnected from broker");
+            }
 
-    public void turnLeft(View view) {
-        System.out.println("l");
+            @Override
+            public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+            }
+        });
     }
-
-    public void stopCar(View view) {
-        System.out.println("s");
-    }
-
 
     private void connectToMqttBroker() {
         if (!isConnected) {
-            mMqttClient.connect(new IMqttActionListener() {
+            mMqttClient.connect(TAG, "", new IMqttActionListener() {
                 @Override
                 public void onSuccess(IMqttToken asyncActionToken) {
                     isConnected = true;
@@ -90,8 +72,8 @@ public class MainActivity extends AppCompatActivity {
                     Log.i(TAG, successfulConnection);
                     Toast.makeText(getApplicationContext(), successfulConnection, Toast.LENGTH_SHORT).show();
 
-                    mMqttClient.subscribe("myfirst/test", QOS, null);
-                    //mMqttClient.subscribe("/smartcar/camera", QOS, null);
+                    mMqttClient.subscribe("/smartcar/ultrasound/front", QOS, null);
+                    mMqttClient.subscribe("/smartcar/camera", QOS, null);
                 }
 
                 @Override
@@ -112,7 +94,22 @@ public class MainActivity extends AppCompatActivity {
 
                 @Override
                 public void messageArrived(String topic, MqttMessage message) throws Exception {
+                    if (topic.equals("/smartcar/camera")) {
+                        final Bitmap bm = Bitmap.createBitmap(IMAGE_WIDTH, IMAGE_HEIGHT, Bitmap.Config.ARGB_8888);
 
+                        final byte[] payload = message.getPayload();
+                        final int[] colors = new int[IMAGE_WIDTH * IMAGE_HEIGHT];
+                        for (int ci = 0; ci < colors.length; ++ci) {
+                            final byte r = payload[3 * ci];
+                            final byte g = payload[3 * ci + 1];
+                            final byte b = payload[3 * ci + 2];
+                            colors[ci] = Color.rgb(r, g, b);
+                        }
+                        bm.setPixels(colors, 0, IMAGE_WIDTH, 0, 0, IMAGE_WIDTH, IMAGE_HEIGHT);
+                        mCameraView.setImageBitmap(bm);
+                    } else {
+                        Log.i(TAG, "[MQTT] Topic: " + topic + " | Message: " + message.toString());
+                    }
                 }
 
                 @Override
@@ -121,5 +118,38 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
+    }
+
+
+    void drive(String topic,String message,String actionDescription){
+        if (!isConnected) {
+            final String notConnected = "Not connected (yet)";
+            Log.e(TAG, notConnected);
+            Toast.makeText(getApplicationContext(), notConnected, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Log.i(TAG, actionDescription);
+        mMqttClient.publish(topic,message,QOS,null);
+    }
+
+
+    public void reverse(View view) {
+        drive("myfirst/test","b","Moving backward");
+    }
+
+    public void turnRight(View view) {
+        drive("myfirst/test","r","Turning right");
+    }
+
+    public void turnLeft(View view) {
+        drive("myfirst/test","l","Turning left");
+    }
+
+    public void forward(View view) {
+        drive("myfirst/test","f","Moving forward");
+    }
+
+    public void stopCar(View view) {
+        drive("myfirst/test","s","Stopping");
     }
 }
--- SmartCarApp/app/src/main/java/com/example/smartcarapp/MqttClient.java
+++ SmartCarApp/app/src/main/java/com/example/smartcarapp/MqttClient.java
@@ -2,21 +2,24 @@ package com.example.smartcarapp;
 
 import android.content.Context;
 
-import android.content.Context;
-
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.*;
 
+// Adapted from: https://medium.com/swlh/android-and-mqtt-a-simple-guide-cb0cbba1931c
+
 public class MqttClient {
+
     private MqttAndroidClient mMqttAndroidClient;
 
     public MqttClient(Context context, String serverUrl, String clientId) {
         mMqttAndroidClient = new MqttAndroidClient(context, serverUrl, clientId);
     }
 
-    public void connect( IMqttActionListener connectionCallback, MqttCallback clientCallback) {
+    public void connect(String username, String password, IMqttActionListener connectionCallback, MqttCallback clientCallback) {
         mMqttAndroidClient.setCallback(clientCallback);
         MqttConnectOptions options = new MqttConnectOptions();
+        options.setUserName(username);
+        options.setPassword(password.toCharArray());
         options.setAutomaticReconnect(true);
         options.setCleanSession(true);
 
